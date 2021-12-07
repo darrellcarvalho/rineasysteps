e7253dcd5b672e3008ae44d6e4e90c5177d03e49
Darrell Carvalho <darrellc1991@gmail.com>
2021-12-07 15:44:44 -0800

    Include quote suppression

---
+++ Hello.R
@@ -0,0 +1,5 @@
+# McGrath's first script is, of course, "Hello, World"
+print("Hello, World!", quote = FALSE) # Mike has us insert quote supression
+
+# Mike helpfully points the reader to be conscious of cursor positioning
+# when using the run-current-line button
