038a1ac7deb96d8e4665a4abc12093b0e7e8116b
Darrell Carvalho <darrellc1991@gmail.com>
2021-12-07 15:44:44 -0800

    Completed "Hello, World!" exercise, pages 18-19

---
+++ Hello.R
@@ -0,0 +1,5 @@
+# McGrath's first script is, of course, "Hello, World"
+print("Hello, World!")
+
+# Mike helpfully points the reader to be conscious of
+# cursor positioning when using the run-current-line button
