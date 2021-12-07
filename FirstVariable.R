54f980475fec08477b7c3ce9fa7f7dd88745ba5e
Darrell Carvalho <darrellc1991@gmail.com>
2021-12-07 15:56:42 -0800

    Exercise on "Storing a single value" pages 22-23

---
+++ FirstVariable.R
@@ -0,0 +1,11 @@
+# This exercise teaches storage of a single variable
+# It also covers the reserved words as a warning to new users
+# It explains how new variables are visible in the Environment Pane in RStudio
+name <- "Username"
+
+# It then has us assign a new value to the variable
+name <- "Darrell"
+
+# It shows how variable calls can be used in other functions through a print
+# demonstration
+print(name)
