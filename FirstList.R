e0fc0d57604c7346ab98e7df248a89f2a93679d0
Darrell Carvalho <darrellc1991@gmail.com>
2021-12-21 21:41:45 -0800

    Getting started with FirstList.R

---
+++ FirstList.R
@@ -0,0 +1,4 @@
+## This exercise focuses on lists and "mixed data types
+# elements of a list can be of any data type, regardless other elements' types
+# lists are indexed at 1 just like vectors.
+data <- list(12, 3.14, "Mike", TRUE)
