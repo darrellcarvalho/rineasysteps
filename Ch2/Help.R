e232bbd0b077aef967a98860bcabcbfa4aa275a1
Darrell Carvalho <darrellc1991@gmail.com>
2022-03-07 10:44:24 -0800

    Finished Help.R

---
+++ Ch2/Help.R
@@ -0,0 +1,9 @@
+## This exercise focuses on methods for getting help within R
+
+# seek help for "color"
+help("color")
+# this first command doesn't work; use ??
+??color
+
+#type plot() and press F1
+plot()
